8057afe25447640ab57ac9a3eb36ba888e239175
Victor Proto <102580400+Victor8411@users.noreply.github.com>
2025-07-01 16:47:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,11 @@ resource "google_compute_instance" "vms" {
     access_config {}
   }
 
+  # Ajout de la clé publique utilisée par Jenkins
+  metadata = {
+    ssh-keys = "admin:${file("${path.module}/jenkins_ansible_key.pub")}"
+  }
+
   metadata_startup_script = <<-EOT
     sudo apt update
     sudo apt install -y python3 python3-pip
@@ -43,6 +48,3 @@ output "vm_ips" {
     name => google_compute_instance.vms[i].network_interface[0].access_config[0].nat_ip
   }
 }
-
-
-
