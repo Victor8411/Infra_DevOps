53a6f00e8913d9a3be35200660a3d0a6f951cbbd
Victor Proto <102580400+Victor8411@users.noreply.github.com>
2025-07-03 15:02:42 +0200

    Update generate-hosts.sh

--- generate-hosts.sh
+++ generate-hosts.sh
@@ -4,13 +4,13 @@ IPS_JSON=$(terraform output -json vm_ips)
 
 cat <<EOF > hosts.ini
 [front]
-$(echo "$IPS_JSON" | jq -r '.front') ansible_user=jenkins ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
+$(echo "$IPS_JSON" | jq -r '.front') ansible_user=vapr84100 ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
 
 [back]
-$(echo "$IPS_JSON" | jq -r '.back') ansible_user=jenkins ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
+$(echo "$IPS_JSON" | jq -r '.back') ansible_user=vapr84100 ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
 
 [db]
-$(echo "$IPS_JSON" | jq -r '.db') ansible_user=jenkins ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
+$(echo "$IPS_JSON" | jq -r '.db') ansible_user=vapr84100 ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa
 EOF
 
 echo "✅ hosts.ini mis à jour"
